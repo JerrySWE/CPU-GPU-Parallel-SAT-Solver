88d699df031fabeaa125a56398c8215fe5e85c3f
Jiarui Wang <jiaruiwa@ghc63.ghc.andrew.cmu.edu>
2022-12-14 02:06:56 -0500

    Refractor legacy code with new generator and verifier

--- cpu_brute_force.cpp
+++ cpu_brute_force.cpp
@@ -6,7 +6,7 @@
 #include "BigIntRepr.h"
 #include <bits/stdc++.h>
 
-void readFile(char *input_file_path, std::vector<int> &CNF, int &v_count, int &c_count) {
+void readFileLegacy(char *input_file_path, std::vector<int> &CNF, int &v_count, int &c_count) {
     std::ifstream infile(input_file_path);
 
     infile >> v_count >> c_count;
@@ -20,9 +20,13 @@ void readFile(char *input_file_path, std::vector<int> &CNF, int &v_count, int &c
         CNF.emplace_back(c2);
         CNF.emplace_back(c3);
     }
+
+    for (int i = 0; i < CNF.size(); i += 3) {
+        std::cout << CNF[i] << " " << CNF[i+1] << " " << CNF[i+2] << std::endl;
+    }
 }
 
-bool solve(BigIntRepr &curr_assignment, std::vector<int> &CNF) {
+bool solveLegacy(BigIntRepr &curr_assignment, std::vector<int> &CNF) {
     int c1, c2, c3;
     for (int i = 0; i < CNF.size(); i+=3) {
         c1 = CNF[i];
@@ -44,7 +48,7 @@ bool solve(BigIntRepr &curr_assignment, std::vector<int> &CNF) {
     return true;
 }
 
-void solveThreadBlock(std::vector<int> &CNF, int v_count, int c_count, char *output_file_path, size_t thread_id, int thread_count, bool *flag) {
+void solveThreadBlockLegacy(std::vector<int> &CNF, int v_count, int c_count, char *output_file_path, size_t thread_id, int thread_count, bool *flag) {
 
     int bit_count = 0;
 
@@ -72,7 +76,7 @@ void solveThreadBlock(std::vector<int> &CNF, int v_count, int c_count, char *out
             break;
         }
 
-        if (solve(bool_assign, CNF)) {
+        if (solveLegacy(bool_assign, CNF)) {
             std::cout << "Solved by thread " << thread_id << std::endl;
             *flag = true;
             std::ofstream outputFile(output_file_path);
@@ -94,11 +98,11 @@ void solveThreadBlock(std::vector<int> &CNF, int v_count, int c_count, char *out
     std::cout << "Thread " << thread_id << " execution time " << duration.count() << " ms\n"; 
 }
 
-void bruteForceSeq(char *input_file_path, char *output_file_path) {
+void bruteForceSeqLegacy(char *input_file_path, char *output_file_path) {
     int v_count, c_count;
     std::vector<int> CNF;
 
-    readFile(input_file_path, CNF, v_count, c_count);
+    readFileLegacy(input_file_path, CNF, v_count, c_count);
 
     auto bool_assign = BigIntRepr(v_count);
 
@@ -106,7 +110,7 @@ void bruteForceSeq(char *input_file_path, char *output_file_path) {
     auto stop = std::chrono::high_resolution_clock::now();
 
     while (true) {
-        if (solve(bool_assign, CNF)) {
+        if (solveLegacy(bool_assign, CNF)) {
             stop = std::chrono::high_resolution_clock::now();
             printf("Solvable!\n");
             std::ofstream outputFile(output_file_path);
@@ -132,11 +136,11 @@ void bruteForceSeq(char *input_file_path, char *output_file_path) {
     std::cout << "Execution time: " << duration.count() << " ms" << std::endl;   
 }
 
-void bruteForceThreadParallel(char *input_file_path, char *output_file_path, int thread_count) {
+void bruteForceThreadParallelLegacy(char *input_file_path, char *output_file_path, int thread_count) {
     // Main thread will read in the data
     int v_count, c_count;
     std::vector<int> CNF;
-    readFile(input_file_path, CNF, v_count, c_count);
+    readFileLegacy(input_file_path, CNF, v_count, c_count);
 
     std::vector<std::thread> threads;
     bool flag = false;
@@ -144,7 +148,7 @@ void bruteForceThreadParallel(char *input_file_path, char *output_file_path, int
     auto stop = std::chrono::high_resolution_clock::now();
 
     for (int i = 0; i < thread_count; i++) {
-        threads.emplace_back(std::thread(solveThreadBlock, std::ref(CNF), v_count, c_count, output_file_path, i, thread_count, &flag));
+        threads.emplace_back(std::thread(solveThreadBlockLegacy, std::ref(CNF), v_count, c_count, output_file_path, i, thread_count, &flag));
     }
 
     for (auto &t : threads) {
@@ -160,6 +164,110 @@ void bruteForceThreadParallel(char *input_file_path, char *output_file_path, int
     stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
     std::cout << "Execution time: " << duration.count() << " ms" << std::endl;   
+}
+
+void readFile(char *input_file_path, std::vector<int> &CNF, int &v_count) {
+    std::ifstream infile(input_file_path);
+    int c_count;
+
+    infile >> v_count >> c_count;
+
+    printf("v_count: %d\n", v_count);
+
+    int c1, c2, c3;
+
+    while (infile >> c1 >> c2 >> c3) {
+        CNF.emplace_back(c1);
+        CNF.emplace_back(c2);
+        CNF.emplace_back(c3);
+    }
+
+    for (int i = 0; i < CNF.size(); i += 3) {
+        std::cout << CNF[i] << " " << CNF[i+1] << " " << CNF[i+2] << std::endl;
+    }
+}
+
+/*
+ * Assignment is represented by unsigned 64 bits integer, each bit will represent the boolean value of a variable
+ * Variable should range from 1-variable count (inclusive)
+ * Note: This representation only support up to 64 boolean variables
+ */
+bool getAssignment(uint64_t assignment, int variable) {
+    // The bit position of the variable in the assignment should be (the absolute value of the variable) - 1 due to 
+    // 0 indexing
+    size_t position = std::abs(variable) - 1;
+    if (variable > 0) {
+        return ((assignment >> position) & 0x1) == 1;
+    } else {
+        return ((assignment >> position) & 0x1) == 0;
+    }
+}
+
+void writeAssignment(char *output_file_path, uint64_t assignment, size_t v_count) {
+    std::ofstream outputFile(output_file_path);
+    for (int i = 1; i < v_count+1; i++) {
+        if (getAssignment(assignment, i)) {
+            outputFile << "Variable: " << std::to_string(i) << " true" << std::endl;
+        } else {
+            outputFile << "Variable: " << std::to_string(i) << " false" << std::endl;
+        }
+    }
+    outputFile.close();
+}
+
+bool solve(uint64_t assignment, std::vector<int> &CNF) {
+    for (int i = 0; i < CNF.size(); i+=3) {
+        int c1 = CNF[i];
+        int c2 = CNF[i+1];
+        int c3 = CNF[i+2];
+
+        if (getAssignment(assignment, c1) || getAssignment(assignment, c2) || getAssignment(assignment, c3)) {
+            continue;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+void solveSeq(std::vector<int> &CNF, size_t v_count, char *output_file_path) {
+    uint64_t base = 1;
+    uint64_t search_space = base << v_count; 
+    for (uint64_t assignment = 0; assignment < search_space; assignment++) {
+        if (solve(assignment, CNF)) {
+            std::cout << "CNF Solved!" << std::endl;
+            writeAssignment(output_file_path, assignment, v_count);
+            return;
+        }
+    }
+    std::cout << "CNF unsolvable!" << std::endl;
+    return;
+}
+
+void solveThreadBlock(std::vector<int> &CNF, size_t v_count, size_t c_count, char *output_file_path, size_t thread_id, size_t thread_count, bool *flag) {
+    uint64_t base = 1;
+    uint64_t search_space = base << v_count; 
+    uint64_t thread_search_space = search_space / thread_count;
+    uint64_t thread_start = thread_id * thread_search_space;
+
+    for (uint64_t assignment = thread_start; assignment < thread_start + thread_search_space; assignment++) {
+
+    }
+}
+
+void solveThreadInterleave(std::vector<int> &CNF, int v_count, int c_count, char *output_file_path, size_t thread_id, size_t thread_count, bool *flag) {
+    uint64_t base = 1;
+    uint64_t search_space = base << v_count; 
+    uint64_t thread_search_space = search_space / thread_count;
+
+    for (uint64_t assignment = thread_id; assignment < search_space; assignment+=thread_count) {
+        
+    }
+}
+
+void solveThreadCentralQueue(std::vector<int> &CNF, int v_count, int c_count, char *output_file_path, size_t thread_id, size_t thread_count, bool *flag) {
+
 }
 
 int main(int argc, char**argv) {
@@ -188,11 +296,15 @@ int main(int argc, char**argv) {
         option = getopt(argc, argv, "i:o:p:");
     }
 
+    std::vector<int> CNF;
+    int v_count;
+    readFile(input_file_path, CNF, v_count);
+
     if (thread_parallel) {
         std::cout << "Parallel execution!" << std::endl;
-        bruteForceThreadParallel(input_file_path, output_file_path, thread_count);
+        // bruteForceThreadParallel(input_file_path, output_file_path, thread_count);
     } else {
-        bruteForceSeq(input_file_path, output_file_path);
+        solveSeq(CNF, v_count, output_file_path);
     }
 
 
--- verifier.py
+++ verifier.py
@@ -5,14 +5,14 @@ import random
 clauseSize = 3
 
 def getBoolVal(assignment, var):
+    pos = abs(var) - 1
+
     if (var < 0):
-        var = abs(var)
-        return ((assignment >> var) & 1) == 0
+        return ((assignment >> pos) & 1) == 0
     else:
-        return ((assignment >> var) & 1) == 1
+        return ((assignment >> pos) & 1) == 1
 
-def verifyUnsatisfiable(testFilePath):
-
+def readCNF(testFilePath):
     f = open(testFilePath, "r")
 
     lines = f.readlines()
@@ -28,7 +28,29 @@ def verifyUnsatisfiable(testFilePath):
         CNF.append(int(clause[0]))
         CNF.append(int(clause[1]))
         CNF.append(int(clause[2]))
-    for assign in range(1<<vCount):
+    
+    f.close()
+    return vCount, CNF
+
+def readSol(solutionPath):
+    f = open(solutionPath, "r")
+
+    lines = f.readlines()
+    solution = list()
+
+    for line in lines:
+        if (line.split(" ")[-1].strip() == "true"):
+            solution.append(True)
+        else:
+            solution.append(False)
+
+    return solution
+
+def verifyUnsatisfiable(testFilePath):
+
+    vCount, CNF = readCNF(testFilePath)
+
+    for assignment in range(1<<vCount):
         satisfiable = True
 
         for i in range(0, len(CNF), 3):
@@ -36,12 +58,41 @@ def verifyUnsatisfiable(testFilePath):
             c2 = CNF[i+1]
             c3 = CNF[i+2]
 
-            
+            if (getBoolVal(assignment, c1) or 
+                getBoolVal(assignment, c2) or
+                getBoolVal(assignment, c3)):
+                continue
+            else:
+                satisfiable = False
+                break
 
-            
+        if (satisfiable):
+            print("CNF satisfiable")
+            return
+    
+    print("CNF unsatisfiable")
 
-def verifySatisfiable():
-    pass
+def verifySatisfiable(testFilePath, solutionFilePath):
+    vCount, CNF = readCNF(testFilePath)
+    sol = readSol(solutionFilePath)
+
+    for i in range(0, len(CNF), 3):
+        c1 = CNF[i]
+        c2 = CNF[i+1]
+        c3 = CNF[i+2]
+
+        c1Pos = abs(c1) - 1
+        c2Pos = abs(c2) - 1
+        c3Pos = abs(c3) - 1
+
+        if ((c1 > 0 and sol[c1Pos]) or (c2 > 0 and sol[c2Pos]) or (c3 > 0 and sol[c3Pos]) or 
+            (c1 < 0 and not sol[c1Pos]) or (c2 < 0 and not sol[c2Pos]) or (c3 < 0 and not sol[c3Pos])):
+            continue
+        print("CNF clause false under assignment!")
+        return
+    
+    print("Solution Legal!")
+
 
 def main(argv):
     testFilePath = ""
@@ -73,6 +124,7 @@ def main(argv):
     else:
         # Verify the authenticity of the solution
         print("Verifying Solution!")
+        verifySatisfiable(testFilePath, assignmentFile)
 
 
 if __name__ == "__main__":
